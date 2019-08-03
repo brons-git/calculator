6d409388bfa84b4da520cdd8a92f203147cb23bf
Bronson <bberwald238@gmail.com>
2019-08-02 19:00:32 -0500

    added functionality for storing numbers in temp output var

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Used for calculations
+    var x:Double = 0;
+    
+    // Number displayed
+    @IBOutlet weak var stored_num: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        stored_num.text = stored_num.text! + String(sender.tag-1)
+        x = Double(stored_num.text!)!
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        // + - X /
+        if stored_num.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            if sender.tag == 12 { //Division
+                
+            } else if sender.tag == 13 { //Multiplication
+                
+            } else if sender.tag == 14 { //Subtraction
+                
+            } else if sender.tag == 15 { //Addition
+                
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
