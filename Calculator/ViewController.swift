271e0e46bc7ed24f114dfbe28e3cd86323980faa
Bronson <bberwald238@gmail.com>
2019-08-04 13:58:40 -0500

    adding calculations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,14 +11,22 @@ import UIKit
 class ViewController: UIViewController {
     
     // Used for calculations
-    var x:Double = 0;
+    var x_placeInFirst:Bool = true;
+    var x_first:Double = 0;
+    var x_second:Double = 0;
+    var calc_char:String = "";
     
     // Number displayed
     @IBOutlet weak var stored_num: UILabel!
     
     @IBAction func numbers(_ sender: UIButton) {
         stored_num.text = stored_num.text! + String(sender.tag-1)
-        x = Double(stored_num.text!)!
+        if x_placeInFirst {
+            x_first = Double(stored_num.text!)!
+        } else {
+            x_second = Double(stored_num.text!)!
+        }
+        x_placeInFirst = !x_placeInFirst
     }
     
     @IBAction func buttons(_ sender: UIButton) {
@@ -26,14 +34,23 @@ class ViewController: UIViewController {
         if stored_num.text != "" && sender.tag != 11 && sender.tag != 16
         {
             if sender.tag == 12 { //Division
+                calc_char = "/"
                 
             } else if sender.tag == 13 { //Multiplication
+                calc_char = "X"
                 
             } else if sender.tag == 14 { //Subtraction
+                calc_char = "-"
                 
             } else if sender.tag == 15 { //Addition
+                calc_char = "+"
                 
             }
+        } else if sender.tag == 11 { //Clear
+            calc_char = "AC"
+            
+        } else if sender.tag == 16 { //Equal
+            calc_char = "="
         }
     }
 
